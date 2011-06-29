5377a25c4324f0a696203b6f30c478dced880306
Dima K <dima@groupon.com>
2011-06-29 14:37:36 -0400

    Make serial projects run first, then run parallel

--- jenkins_conductor.rb
+++ jenkins_conductor.rb
@@ -12,6 +12,44 @@ params = ARGV.getopts("c:", "current_job:")
 root_job = params["c"].nil? ? params["current_job"] : params["c"]
 
 
+config["jobs"][root_job]["downstream_jobs"]["serial_jobs"].each do |serial_project|
+  current_job_name = serial_project.keys.first
+  url_to_job = "#{config["jenkins_base_url"]}/view/All/job/#{current_job_name}"
+  all_jobs_for_current_project = JSON.parse(Net::HTTP.get_response(URI.parse(url_to_job + "/api/json")).body)
+
+  previous_job_number = all_jobs_for_current_project["builds"].first["number"]
+  current_job_number = previous_job_number + 1
+
+  uri = URI.parse("#{url_to_job}/buildWithParameters")
+
+  http = Net::HTTP.new(uri.host, uri.port)
+
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.set_form_data(serial_project[current_job_name]["params"])
+
+  response = http.request(request)
+  throw "Error with post to #{uri.to_s} with #{serial_project[current_job_name]["params"]} return was not 'Net::HTTPFound 302 Found'" unless response.kind_of?(Net::HTTPFound)
+
+  puts "Waiting for job #{current_job_name} build #{current_job_number} to start"
+  begin
+    current_job_start_status = Net::HTTP.get_response(URI.parse(url_to_job + "/#{current_job_number}/api/json"))
+    sleep 5
+  end while current_job_start_status.kind_of?(Net::HTTPNotFound)
+
+  puts "Job #{current_job_name} build #{current_job_number} started"
+
+  puts "waiting for job #{current_job_name} build #{current_job_number} to finish"
+  begin
+    current_job = JSON.parse(Net::HTTP.get_response(URI.parse(url_to_job + "/#{current_job_number}/api/json")).body)
+    sleep 5
+  end while current_job["result"].nil?
+
+  puts "Job #{current_job_name} build #{current_job_number} with result of #{current_job['result']}"
+end
+
+
+
+
 downstream_parallel_jobs_to_run = config["jobs"][root_job]["downstream_jobs"]["parallel_jobs"]
 
 Parallel.map(downstream_parallel_jobs_to_run, :in_threads => downstream_parallel_jobs_to_run.size) do |current_project|
@@ -22,6 +60,7 @@ Parallel.map(downstream_parallel_jobs_to_run, :in_threads => downstream_parallel
 
   all_jobs_for_current_project = JSON.parse(Net::HTTP.get_response(URI.parse(url_to_job + "/api/json")).body)
   previous_job_number = all_jobs_for_current_project["builds"].first["number"]
+  current_job_number = previous_job_number + 1
 
   uri = URI.parse("#{url_to_job}/buildWithParameters")
 
@@ -34,8 +73,6 @@ Parallel.map(downstream_parallel_jobs_to_run, :in_threads => downstream_parallel
   response = http.request(request)
   throw "Error with post to #{uri.to_s} with #{current_project[current_job_name]["params"]} return was not 'Net::HTTPFound 302 Found'" unless response.kind_of?(Net::HTTPFound)
 
-
-  current_job_number = previous_job_number + 1
 
   puts "Waiting for job #{current_job_name} build #{current_job_number} to start"
   begin
