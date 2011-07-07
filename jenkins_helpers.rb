de430190f191027f58c5b21952af11e141b9c7b9
Dima K <dima@groupon.com>
2011-07-07 11:49:56 -0400

    Refactor: Make all gets and post happen in 1 place, add basic auth to the gets and posts if one is needed

--- jenkins_helpers.rb
+++ jenkins_helpers.rb
@@ -26,7 +26,7 @@ def launch_project_and_monitor_progress(current_project)
 end
 
 def get_all_builds_for_project(url_to_job)
-  JSON.parse(Net::HTTP.get_response(URI.parse(url_to_job + "/api/json")).body)
+  JSON.parse(make_get_request(url_to_job + "/api/json").body)
 end
 
 def get_current_build_number(all_jobs_for_current_project)
@@ -35,21 +35,14 @@ def get_current_build_number(all_jobs_for_current_project)
 end
 
 def trigger_job(url_to_job, params)
-  uri = URI.parse("#{url_to_job}/buildWithParameters")
-
- http = Net::HTTP.new(uri.host, uri.port)
-
- request = Net::HTTP::Post.new(uri.request_uri)
- request.set_form_data(params)
-
- response = http.request(request)
- throw "Error with post to #{uri.to_s} with #{url_to_job} return was not 'Net::HTTPFound 302 Found'" unless response.kind_of?(Net::HTTPFound)
+ response = make_post_request("#{url_to_job}/buildWithParameters", params)
+ throw "Error with post to #{url_to_job} with #{params} return was not 'Net::HTTPFound 302 Found'" unless response.kind_of?(Net::HTTPFound)
 end
 
 def wait_for_build_to_start(url_to_job, current_job_name, current_build_number)
   puts "Waiting for job #{current_job_name} build #{current_build_number} to start"
   begin
-    current_job_start_status = Net::HTTP.get_response(URI.parse(url_to_job + "/#{current_build_number}/api/json"))
+    current_job_start_status = make_get_request(url_to_job + "/#{current_build_number}/api/json")
     sleep 5
   end while current_job_start_status.kind_of?(Net::HTTPNotFound)
 
@@ -59,7 +52,7 @@ end
 def wait_for_build_to_finish(url_to_job, current_job_name, current_build_number)
   puts "waiting for job #{current_job_name} build #{current_build_number} to finish"
   begin
-    current_job = JSON.parse(Net::HTTP.get_response(URI.parse(url_to_job + "/#{current_build_number}/api/json")).body)
+    current_job = JSON.parse(make_get_request(url_to_job + "/#{current_build_number}/api/json").body)
     sleep 5
   end while current_job["result"].nil?
   puts "Job #{current_job_name} build #{current_build_number} with result of #{current_job['result']}"
@@ -71,7 +64,7 @@ def get_artifact_from_job(project, url_to_job)
   artifacts_to_get = project[project.keys.first]["test_result_artifacts"]
   return "No artifact to get from config" unless artifacts_to_get
 
-  job = JSON.parse(Net::HTTP.get_response(URI.parse(url_to_job + "/api/json")).body)
+  job = JSON.parse(make_get_request(url_to_job + "/api/json").body)
   return "Project didn't have any artifacts" if job["artifacts"].empty?
 
   files_to_download = artifacts_to_get.collect do |current_artifact|
@@ -100,12 +93,7 @@ def download_file(url)
   url =~ /([\w\.]*)$/
   file = $1
 
-  uri = URI.parse(url)
-
-  http = Net::HTTP.new(uri.host, uri.port)
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-
+  response = make_get_request url
   open("#{@artifact_dir}/#{file}", "wb") {|save_file| save_file.write(response.body)}
 end
 
@@ -119,4 +107,46 @@ def unzip_file(url)
   `cd #{@artifact_dir}; tar xvf #{file}; cd -`
   `rm -rf #{@artifact_dir}/#{file}`
  end
+end
+
+def make_get_request(url)
+ uri = URI.parse(url)
+
+ http = Net::HTTP.new(uri.host, uri.port)
+
+ request = Net::HTTP::Get.new(uri.request_uri)
+
+ if get_basic_auth_credentials
+   username, password = get_basic_auth_credentials
+   request.basic_auth username, password
+ end
+
+ http.request(request)
+end
+
+def make_post_request(url, params)
+ uri = URI.parse(url)
+
+ http = Net::HTTP.new(uri.host, uri.port)
+
+ request = Net::HTTP::Post.new(uri.request_uri)
+
+ if get_basic_auth_credentials
+   username, password = get_basic_auth_credentials
+   request.basic_auth username, password
+ end
+
+ request.set_form_data(params)
+
+ http.request(request)
+end
+
+def get_basic_auth_credentials
+
+  username = @config["basic_auth"]["username"]
+  password = @config["basic_auth"]["password"]
+
+  unless username.nil? or password.nil?
+    return username, password
+  end
 end
