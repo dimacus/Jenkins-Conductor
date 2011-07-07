fa77862186133c3e9789bcdf0cfcb9f46c454d2d
Dima K <dima@groupon.com>
2011-07-07 11:11:20 -0400

    Return job status from launch job function. Add methods to download and unzip artifacts from a job

--- jenkins_helpers.rb
+++ jenkins_helpers.rb
@@ -13,15 +13,16 @@ def launch_project_and_monitor_progress(current_project)
 
   @all_job_statuses[current_job_name] = {:result => result,
                                         :test_result_artifact => current_project[current_job_name]["test_result_artifact"] }
-
-  return true if result.upcase == "SUCCESS"
+  return "pass", "#{url_to_job}/#{current_build_number}" if result.upcase == "SUCCESS"
 
   if current_project[current_job_name]["continue_on_fail"] == false
     puts "#{current_job_name} was explicitly set to exit on failure.\n\n"
     puts "Current project statuses\n #{@all_job_statuses.inspect}\n\n"
     puts "#{current_job_name} configs\n #{current_project.inspect}\n\n"
-    exit 1
+    return "fail", "#{url_to_job}/#{current_build_number}"
   end
+
+  return "fail-continue", "#{url_to_job}/#{current_build_number}"
 end
 
 def get_all_builds_for_project(url_to_job)
@@ -29,7 +30,7 @@ def get_all_builds_for_project(url_to_job)
 end
 
 def get_current_build_number(all_jobs_for_current_project)
-  previous_job_number = all_jobs_for_current_project["builds"].first.empty? ? all_jobs_for_current_project["builds"].first["number"] : 0
+  previous_job_number = all_jobs_for_current_project["builds"].first.empty? ?  0 : all_jobs_for_current_project["builds"].first["number"]
   previous_job_number + 1
 end
 
@@ -42,7 +43,7 @@ def trigger_job(url_to_job, params)
  request.set_form_data(params)
 
  response = http.request(request)
- throw "Error with post to #{uri.to_s} with #{current_project[current_job_name]["params"]} return was not 'Net::HTTPFound 302 Found'" unless response.kind_of?(Net::HTTPFound)
+ throw "Error with post to #{uri.to_s} with #{url_to_job} return was not 'Net::HTTPFound 302 Found'" unless response.kind_of?(Net::HTTPFound)
 end
 
 def wait_for_build_to_start(url_to_job, current_job_name, current_build_number)
@@ -64,4 +65,58 @@ def wait_for_build_to_finish(url_to_job, current_job_name, current_build_number)
   puts "Job #{current_job_name} build #{current_build_number} with result of #{current_job['result']}"
 
   current_job["result"]
+end
+
+def get_artifact_from_job(project, url_to_job)
+  artifacts_to_get = project[project.keys.first]["test_result_artifacts"]
+  return "No artifact to get from config" unless artifacts_to_get
+
+  job = JSON.parse(Net::HTTP.get_response(URI.parse(url_to_job + "/api/json")).body)
+  return "Project didn't have any artifacts" if job["artifacts"].empty?
+
+  files_to_download = artifacts_to_get.collect do |current_artifact|
+    matched_artifact_info = job["artifacts"].select do |published_artifact|
+      published_artifact["fileName"] == current_artifact
+    end.first
+
+    "#{url_to_job}/artifact/#{matched_artifact_info['relativePath']}"
+  end
+
+
+
+  unless files_to_download.empty?
+    files_to_download.each do |file|
+      download_file(file)
+      unzip_file(file)
+    end
+  end
+
+  "Artifact(s) downloaded"
+
+end
+
+def download_file(url)
+  puts "Downloading #{url}"
+  url =~ /([\w\.]*)$/
+  file = $1
+
+  uri = URI.parse(url)
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+
+  open("#{@artifact_dir}/#{file}", "wb") {|save_file| save_file.write(response.body)}
+end
+
+def unzip_file(url)
+
+  url =~ /([\w\.]*)$/
+  file = $1
+
+ if file.include? ".zip"
+   puts "Unzipping #{file}"
+  `cd #{@artifact_dir}; tar xvf #{file}; cd -`
+  `rm -rf #{@artifact_dir}/#{file}`
+ end
 end
