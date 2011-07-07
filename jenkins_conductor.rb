4affdd62d6361ac6fd1db56d80f50c8cdc2fa251
Dima K <dima@groupon.com>
2011-07-07 11:10:33 -0400

    Clean up artifact dir, check if there are any tasks to run before running them. Trigger download artifact from a different job

--- jenkins_conductor.rb
+++ jenkins_conductor.rb
@@ -8,6 +8,10 @@ require "parallel"
 
 require "jenkins_helpers"
 
+@artifact_dir = "artifacts"
+
+`rm -rf #{@artifact_dir}`
+`mkdir #{@artifact_dir}`
 
 @config = YAML.load_file("jenkins_conductor_config.yml")
 cli_params = ARGV.getopts("c:", "current_job:")
@@ -16,15 +20,22 @@ root_job = cli_params["c"].nil? ? cli_params["current_job"] : cli_params["c"]
 
 @all_job_statuses = Hash.new
 
-@config["jobs"][root_job]["downstream_jobs"]["serial_jobs"].each do |serial_project|
-  launch_project_and_monitor_progress(serial_project)
+serial_jobs = @config["jobs"][root_job]["downstream_jobs"]["serial_jobs"]
+unless serial_jobs.nil? or serial_jobs.empty?
+  serial_jobs.each do |serial_project|
+    status, link_to_job = launch_project_and_monitor_progress(serial_project)
+    result = get_artifact_from_job(serial_project, link_to_job)
+    puts result
+  end
 end
 
 
-
-downstream_parallel_jobs_to_run = @config["jobs"][root_job]["downstream_jobs"]["parallel_jobs"]
-Parallel.map(downstream_parallel_jobs_to_run, :in_threads => downstream_parallel_jobs_to_run.size) do |current_project|
-  launch_project_and_monitor_progress(current_project)
+parallel_jobs_to_run = @config["jobs"][root_job]["downstream_jobs"]["parallel_jobs"]
+unless parallel_jobs_to_run.nil? or parallel_jobs_to_run.empty?
+  Parallel.map(parallel_jobs_to_run, :in_threads => parallel_jobs_to_run.size) do |current_project|
+    status, link_to_job = launch_project_and_monitor_progress(current_project)
+    result = get_artifact_from_job(current_project, link_to_job)
+    puts result
+  end
 end
-
 puts @all_job_statuses.inspect
