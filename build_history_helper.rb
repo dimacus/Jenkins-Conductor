7f6611fc018d57e8263c270298a7228d7a6bb4e6
Dima Kovalenko <dima@dima-iMac.local>
2011-07-19 15:24:21 -0400

    Add helper to take care of build history for a given project

---
+++ build_history_helper.rb
@@ -0,0 +1,23 @@
+module BuildHistoryHelper
+
+  def get_all_builds_for_job
+    JSON.parse(make_get_request(url + "/api/json").body)["builds"]
+  end
+
+  def is_current_job?(job_info, parent_build_id, parent_job_name)
+    historical_build = get_historical_build_info("#{url}/#{job_info["number"]}")
+    build_match      = historical_build["parent_job_build_id"].to_i == parent_build_id.to_i
+    project_match    = historical_build["parent_job_name"]          == parent_job_name
+
+    build_match == true and project_match == true
+  end
+
+  def get_historical_build_info(url_to_build)
+    parameters_to_return = {}
+    job_info = JSON.parse(make_get_request(url_to_build + "/api/json").body)
+    job_info["actions"].first["parameters"].each {|parameter| parameters_to_return[parameter["name"]] = parameter["value"] }
+
+    parameters_to_return
+  end
+
+end
